7cb77ac289712b66044589ef1e8bc11bf24c8048
Jason Lee <jason.lee@cern.ch>
2015-11-18 03:03:00 +0900

    pythia6 test

--- QCD_TuneZ2star_13TeV_pythia6_rivet.py
+++ QCD_TuneZ2star_13TeV_pythia6_rivet.py
@@ -39,7 +39,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     'keep recoGenJets_ak4GenJetsNoNu_*_*',
     #'keep *_genParticles_*_*',
     ),
-    fileName = cms.untracked.string('QCD_TuneCUETP8M1_13TeV_pythia8_GEN.root'),
+    fileName = cms.untracked.string('QCD_TuneZ2star_13TeV_pythia6_rivet.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM-RAW')
@@ -56,35 +56,35 @@ process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
 
-from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.PythiaUEZ2starSettings_cfi import *
 
-process.generator = cms.EDFilter("Pythia8GeneratorFilter",
-	maxEventsToPrint = cms.untracked.int32(1),
-	pythiaPylistVerbosity = cms.untracked.int32(1),
-	filterEfficiency = cms.untracked.double(1.0),
-	pythiaHepMCVerbosity = cms.untracked.bool(False),
+process.generator = cms.EDFilter('Pythia6GeneratorFilter',
 	comEnergy = cms.double(13000.0),
-	crossSection = cms.untracked.double(0.84265),
+	crossSection = cms.untracked.double(0),
+	filterEfficiency = cms.untracked.double(1),
+	maxEventsToPrint = cms.untracked.int32(0),
+	pythiaHepMCVerbosity = cms.untracked.bool(False),
+	pythiaPylistVerbosity = cms.untracked.int32(0),
+
 	PythiaParameters = cms.PSet(
-            pythia8CommonSettingsBlock,
-            pythia8CUEP8M1SettingsBlock,
-	    processParameters = cms.vstring(
-			'HardQCD:3parton = on',
-			'PhaseSpace:pTHatMin = 100  ',
-			'PhaseSpace:pTHatMax = -1  ',
-            #'TimeShower:globalRecoil = on', # might turn off colour coherence
-	    ),
-            parameterSets = cms.vstring('pythia8CommonSettings',
-                                        'pythia8CUEP8M1Settings',
-                                        'processParameters',
-                                        )
+		pythiaUESettingsBlock,
+		processParameters = cms.vstring(
+			'MSEL = 1        ! QCD hight pT processes',
+			'CKIN(3) = 50    ! minimum pt hat for hard interactions',
+			'CKIN(4) = 10000    ! maximum pt hat for hard interactions',
+            'MSTP(67) = 0',
+            'MSTJ(50) = 0',
+		),
+		parameterSets = cms.vstring(
+			'pythiaUESettings',
+			'processParameters',
+		)
 	)
 )
 
 process.load('GeneratorInterface.RivetInterface.rivetAnalyzer_cfi')
 process.rivetAnalyzer.AnalysisNames = cms.vstring('cc_ana')
-process.rivetAnalyzer.OutputFile = cms.string('QCD_TuneCUETP8M1_13TeV_pythia8_rivet.yoda')
+process.rivetAnalyzer.OutputFile = cms.string('QCD_TuneZ2star_13TeV_pythia6_rivet.yoda')
 
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
